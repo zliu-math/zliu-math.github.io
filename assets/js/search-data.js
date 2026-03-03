56fd8c9abc303d6a4d4c70b4a39739d42a37a0aa
zliu-math <ziran.github@gmail.com>
2026-03-03 17:48:50 +0000

    Deploying to gh-pages from @ zliu-math/zliu-math.github.io@00864a258e8e207f645798cfb342dc49414fd12a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -440,8 +440,8 @@ ninja.data = [{
           description: "这里写你想加的那段文字内容。例如：这是我关于概率论与深度学习结合的研究项目，重点在于 LCK 框架的应用。",
           section: "Projects",handler: () => {
               window.location.href = "/projects/TwoQueues/";
-            },},{id: "projects-submartingale-problem-for-reflected-brownian-motion",
-          title: 'Submartingale Problem for Reflected Brownian Motion',
+            },},{id: "projects-submartingale-problem-for-rbm-with-drift-in-a-wedge",
+          title: 'Submartingale Problem for RBM with Drift in a Wedge',
           description: "A study on the existence, uniqueness, and path properties of RBM in wedge domains via the Varadhan-Williams submartingale formulation.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/subMG_RBM/";
