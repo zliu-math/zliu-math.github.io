433b7bb7038cc2804a4fd35b24b5b427776cde82
zliu-math <ziran.github@gmail.com>
2026-03-03 20:03:19 +0000

    Deploying to gh-pages from @ zliu-math/zliu-math.github.io@32716ab997e7eb30c30dece0b95ca48411f1c570 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -425,9 +425,9 @@ ninja.data = [{
           description: "A project on the Riemannian geometry induced by Jacobian chains in deep nonlinear networks—linking depth, curvature, stability, and trainability.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Jacobian/";
-            },},{id: "projects-prescribed-gaussian-curvature-by-discrete-conformality",
-          title: 'Prescribed Gaussian Curvature by Discrete Conformality',
-          description: "A convex-variational, discrete-conformal method that approximates the conformal factor solving the prescribed Gaussian curvature equation on closed genus &gt; 1 surfaces, with a sharp first-order convergence guarantee.",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/PresGauCur/";
             },},{id: "projects-我的核心项目",
