65e951921c7826295df9db680ea40e63fe4d8adc
zliu-math <ziran.github@gmail.com>
2026-03-03 17:19:37 +0000

    Deploying to gh-pages from @ zliu-math/zliu-math.github.io@95d18c9c8f511f27dcbc9d569f5c24ec0b1fc26e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -420,9 +420,9 @@ ninja.data = [{
           description: "这里写你想加的那段文字内容。例如：这是我关于概率论与深度学习结合的研究项目，重点在于 LCK 框架的应用。",
           section: "Projects",handler: () => {
               window.location.href = "/projects/GCh/";
-            },},{id: "projects-symmetry-reduction-and-entropy-in-deep-networks",
-          title: 'Symmetry, Reduction, and Entropy in Deep Networks',
-          description: "A geometric–statistical physics program for deep learning, from linear solvable limits to nonlinear Jacobian-chain geometry and depth scaling laws.",
+            },},{id: "projects-nonlinear-jacobian-chain-geometry",
+          title: 'Nonlinear Jacobian Chain Geometry',
+          description: "A project on the Riemannian geometry induced by Jacobian chains in deep nonlinear networks—linking depth, curvature, stability, and trainability.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/Jacobian/";
             },},{id: "projects-我的核心项目",
