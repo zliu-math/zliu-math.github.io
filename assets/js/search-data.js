22a69056a1be7f8f8071909f1af768fbedb8e84d
zliu-math <ziran.github@gmail.com>
2026-03-03 15:51:54 +0000

    Deploying to gh-pages from @ zliu-math/zliu-math.github.io@db2322d78cbe14c982b8cd0638f94817e13a1a01 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -435,9 +435,9 @@ ninja.data = [{
           description: "这里写你想加的那段文字内容。例如：这是我关于概率论与深度学习结合的研究项目，重点在于 LCK 框架的应用。",
           section: "Projects",handler: () => {
               window.location.href = "/projects/TwoQueues/";
-            },},{id: "projects-我的核心项目",
-          title: '我的核心项目',
-          description: "这里写你想加的那段文字内容。例如：这是我关于概率论与深度学习结合的研究项目，重点在于 LCK 框架的应用。",
+            },},{id: "projects-nonlinear-jacobian-chain-geometry",
+          title: 'Nonlinear Jacobian Chain Geometry',
+          description: "A project on the Riemannian geometry induced by Jacobian chains in deep nonlinear networks—linking depth, curvature, stability, and trainability.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/my_work/";
             },},{id: "projects-我的核心项目",
