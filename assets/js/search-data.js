41da22ed4146f8751c3dcde69a6dc22d4482d1ae
zliu-math <ziran.github@gmail.com>
2026-03-13 23:56:02 +0000

    Deploying to gh-pages from @ zliu-math/zliu-math.github.io@d4a8ebf987ea4da75a8dd42049a25492b3a41314 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,6 +16,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/research/";
           },
+        },{id: "nav-research",
+          title: "Research",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
         },{id: "nav-projects",
           title: "Projects",
           description: "Current Projects of Research and Patents",
