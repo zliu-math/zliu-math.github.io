62138e9811e48e38da973f8ada520babc38f1829
zliu-math <ziran.github@gmail.com>
2026-03-13 22:18:52 +0000

    Deploying to gh-pages from @ zliu-math/zliu-math.github.io@006dd7dab246fa5d04c430736fbc242f2f720936 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -42,7 +42,7 @@ ninja.data = [{
           description: "Materials for courses you taught. Replace this text with your description.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/teaching/";
+            window.location.href = "/t_eaching/";
           },
         },{id: "post-a-post-with-plotly-js",
         
